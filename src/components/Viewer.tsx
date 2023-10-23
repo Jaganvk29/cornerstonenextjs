7c0e8c3507dce235bded7b6445d38cdfe36b00ec
jaganvk29 <jaganvk29@gmail.com>
2023-10-23 23:27:32 +0530

    first commit

---
+++ src/components/Viewer.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { RenderingEngine, Types, Enums } from "@cornerstonejs/core";
+import {
+  initDemo,
+  createImageIdsAndCacheMetaData,
+  setTitleAndDescription,
+  ctVoiRange,
+} from "@/utils/demo/helpers";
+const { ViewportType } = Enums;
+
+type Props = {};
+
+const Viewer = (props: Props) => {
+  async function run() {
+    setTitleAndDescription(
+      "Basic Stack",
+      "Displays a single DICOM image in a Stack viewport."
+    );
+
+    const content = document.getElementById("content");
+    const element = document.createElement("div");
+    element.id = "cornerstone-element";
+    element.style.width = "500px";
+    element.style.height = "500px";
+
+    content.appendChild(element);
+    // Init Cornerstone and related libraries
+    await initDemo();
+
+    // Get Cornerstone imageIds and fetch metadata into RAM
+    const imageIds = await createImageIdsAndCacheMetaData({
+      StudyInstanceUID:
+        "1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463",
+      SeriesInstanceUID:
+        "1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561",
+      wadoRsRoot: "https://d3t6nz73ql33tx.cloudfront.net/dicomweb",
+    });
+
+    // Instantiate a rendering engine
+    const renderingEngineId = "myRenderingEngine";
+    const renderingEngine = new RenderingEngine(renderingEngineId);
+
+    // Create a stack viewport
+    const viewportId = "CT_STACK";
+    const viewportInput = {
+      viewportId,
+      type: ViewportType.STACK,
+      element,
+      defaultOptions: {
+        background: [0.2, 0, 0.2],
+      },
+    };
+
+    renderingEngine.enableElement(viewportInput);
+
+    // Get the stack viewport that was created
+    const viewport = renderingEngine.getViewport(viewportId);
+
+    // Define a stack containing a single image
+    const stack = [imageIds[0]];
+
+    // Set the stack on the viewport
+    await viewport.setStack(stack);
+
+    // Set the VOI of the stack
+    viewport.setProperties({ voiRange: ctVoiRange });
+
+    // Render the image
+    viewport.render();
+  }
+
+  useEffect(() => {
+    run();
+  }, []);
+  return <div id="content">Viewer</div>;
+};
+
+export default Viewer;
